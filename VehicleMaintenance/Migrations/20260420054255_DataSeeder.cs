f4ff31e57a7e113735a453449f288b635b7c8040
Orest Hevorhian <o.hevorhyan@gmail.com>
2026-04-21 01:13:14 +0200

    feat(migrations): add migrations data seeder - add migrations data seeder

---
+++ VehicleMaintenance/Migrations/20260420054255_DataSeeder.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VehicleMaintenance.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeeder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Predictions_VehicleComponents_VehicleComponentId",
+                table: "Predictions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VehicleComponentId",
+                table: "Predictions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Predictions_VehicleComponents_VehicleComponentId",
+                table: "Predictions",
+                column: "VehicleComponentId",
+                principalTable: "VehicleComponents",
+                principalColumn: "VehicleComponentId",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Predictions_VehicleComponents_VehicleComponentId",
+                table: "Predictions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VehicleComponentId",
+                table: "Predictions",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Predictions_VehicleComponents_VehicleComponentId",
+                table: "Predictions",
+                column: "VehicleComponentId",
+                principalTable: "VehicleComponents",
+                principalColumn: "VehicleComponentId");
+        }
+    }
+}
